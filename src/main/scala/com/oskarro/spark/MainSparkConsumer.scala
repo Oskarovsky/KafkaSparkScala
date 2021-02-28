2c5ad5da5d674dd29507ac8e643081d0017efc1d
Oskarovsky <oskar.slyk@gmail.com>
2021-02-28 21:11:28 +0100

    agg sparl

--- src/main/scala/com/oskarro/spark/MainSparkConsumer.scala
+++ src/main/scala/com/oskarro/spark/MainSparkConsumer.scala
@@ -3,7 +3,7 @@ package com.oskarro.spark
 import com.datastax.oss.driver.api.core.uuid.Uuids
 import com.oskarro.config.Constants
 import org.apache.spark.sql.cassandra.DataFrameWriterWrapper
-import org.apache.spark.sql.functions.{from_json, to_timestamp, udf}
+import org.apache.spark.sql.functions.{atan2, cos, from_json, lag, pow, sin, sqrt, toRadians, to_timestamp, udf, unix_timestamp}
 import org.apache.spark.sql.streaming.Trigger
 import org.apache.spark.sql.types.{StringType, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -23,6 +23,7 @@ object MainSparkConsumer {
 
   def readCurrentLocationOfVehicles(topic: String, kafkaServers: String,
                                     cassandraKeyspace: String, cassandraTable: String): Unit = {
+    // Spark Session create
     val spark = SparkSession
       .builder()
       .appName(Constants.appName)
@@ -42,6 +43,7 @@ object MainSparkConsumer {
       .add("Lat", "string")
       .add("Brigade", "string")
 
+    // Read data from Kafka topic
     val inputDf = spark
       .readStream
       .format("kafka")
@@ -49,16 +51,34 @@ object MainSparkConsumer {
       .option("subscribe", topic)
       .load()
 
-      val trafficStream = inputDf
-        .withColumn("traffic", from_json($"value".cast(StringType), jsonSchema))
-        .selectExpr("traffic.*", "partition", "offset")
+    val makeUUID = udf(() => Uuids.timeBased().toString)
 
-      val makeUUID = udf(() => Uuids.timeBased().toString)
+    val trafficStream = inputDf
+      .withColumn("traffic", from_json($"value".cast(StringType), jsonSchema))
+      .selectExpr("traffic.*", "partition", "offset")
 
-      val summaryWithIDs = trafficStream
-        .withColumn("uuid", makeUUID())
-        .withColumn("Time", to_timestamp($"time"))
+    val summaryWithIDs = trafficStream
+      .withColumn("uuid", makeUUID())
+      .withColumn("Time", to_timestamp($"Time"))
 
+
+    // TODO
+/*    val w = org.apache.spark.sql.expressions.Window
+      .partitionBy("Lines", "Brigade")
+      .orderBy("Time")
+
+    summaryWithIDs
+      .where("Time is not null and Lines != 121")
+      .withColumn("prev_lat", lag("Lat", 1, 0))
+      .withColumn("prev_long", lag("Lon", 1, 0))
+      .withColumn("prev_time", lag("Time", 1))
+      .withColumn("a", pow(sin(toRadians($"Lat" - $"prev_lat") / 2), 2) + cos(toRadians($"prev_lat")) * cos(toRadians($"Lat")) * pow(sin(toRadians($"Lon" - $"prev_long") / 2), 2))
+      .withColumn("distance", atan2(sqrt($"a"), sqrt(-$"a" + 1)) * 2 * 6371)
+      .withColumn("time_diff", unix_timestamp($"Time") - unix_timestamp($"prev_time"))
+      .withColumn("speed", $"distance"/$"time_diff" * 3600)
+      .writeStream
+      .format("console")
+      .start().awaitTermination()*/
 
       val query = summaryWithIDs
         .writeStream
